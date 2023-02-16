ed2eb1596fbf0ab67986b7ec7a0bdb1bd0caf74a
Adrian Medina <adrian@wikipunk.net>
2023-02-16 17:40:30 -0500

    init

---
+++ src/net/wikipunk/punk/qudt/boot.clj
@@ -0,0 +1,7 @@
+(ns net.wikipunk.punk.qudt.boot
+  {:rdf/type :jsonld/Context})
+
+(def punk.qudt
+  {:rdf/type    :rdfa/PrefixMapping
+   :rdfa/uri    "https://wikipunk.net/qudt/"
+   :rdfa/prefix "punk.qudt"})
---
+++ src/net/wikipunk/rdf/punk/qudt.clj
@@ -0,0 +1,8 @@
+(ns net.wikipunk.rdf.punk.qudt
+  {:rdf/type :owl/Ontology})
+
+;; Begin modelling your future hyperreality today!
+
+(def Test
+  {:db/ident :punk.qudt/Test
+   :rdf/type :owl/Class})
